9e94cd3b54b32fdf80383ebd9dc1ec13db68a45a
Dabana Intenque <dabanaluintenque55@gmail.com>
2022-02-25 16:39:27 -0500

    first file

---
+++ external.js
@@ -0,0 +1,12 @@
+
+
+
+ function myFunction(value) {
+           document.getElementById('header').innerHTML = "change the content of the header.";
+            document.getElementById('demo').innerHTML = "change the content of the paragrath.";
+
+      //document.getElementById('demo').textContent=value;      
+    
+ }
+
+   
